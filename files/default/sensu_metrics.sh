e6ce4e62a3a6273f7e1e054c33aa7095d5aff6a3
Scott Medefind <smedefind@optoro.com>
2015-04-27 15:06:36 -0400

    Adding nginx metrics recipe

--- files/default/sensu_metrics.sh
+++ files/default/sensu_metrics.sh
@@ -4,6 +4,8 @@ NETCAT="/bin/nc -q0 -t 127.0.0.1 6008"
 CLEAN_OUTPUT="grep ."
 SENSU_SCRIPT=$1
 
+shift
+
 if [[ $SENSU_SCRIPT == *".rb" ]]; then
   if [ -a /usr/local/rvm/scripts/rvm ]; then
     . /usr/local/rvm/scripts/rvm
@@ -12,7 +14,7 @@ if [[ $SENSU_SCRIPT == *".rb" ]]; then
 
   # grep is used to remove an empty line emitted after every script runs.
   # It leads to a crash of logstash.
-  $SENSU_PATH/embedded/bin/ruby $SENSU_PATH/plugins/$SENSU_SCRIPT | $CLEAN_OUTPUT | $NETCAT
+  $SENSU_PATH/embedded/bin/ruby $SENSU_PATH/plugins/$SENSU_SCRIPT $@ | $CLEAN_OUTPUT | $NETCAT
 else
   $SENSU_PATH/plugins/$SENSU_SCRIPT | $CLEAN_OUTPUT | $NETCAT
 fi

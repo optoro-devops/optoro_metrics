37329f9f5bfa8406a9f7df28c53a70ec4a1d69ae
Scott Medefind <smedefind@optoro.com>
2015-06-18 16:25:43 -0400

    Adding cpu usage as a percent metric

--- recipes/system.rb
+++ recipes/system.rb
@@ -4,7 +4,7 @@
 
 include_recipe 'optoro_metrics::default'
 
-%w(cpu-metrics.rb disk-capacity-metrics.rb disk-metrics.rb disk-usage-metrics.rb interface-metrics.rb iostat-extended-metrics.rb load-metrics.rb memory-metrics-percent.rb memory-metrics.rb ntpdate-metrics.rb ntpstats-metrics.rb vmstat-metrics.rb).each do |script|
+%w(cpu-pcnt-usage-metrics.rb cpu-metrics.rb disk-capacity-metrics.rb disk-metrics.rb disk-usage-metrics.rb interface-metrics.rb iostat-extended-metrics.rb load-metrics.rb memory-metrics-percent.rb memory-metrics.rb ntpdate-metrics.rb ntpstats-metrics.rb vmstat-metrics.rb).each do |script|
   cron "poll_#{script}" do
     action :create
     command "/opt/logstash/agent/bin/sensu_metrics.sh system/#{script}"
